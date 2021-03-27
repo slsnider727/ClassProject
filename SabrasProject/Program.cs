8ec71477b271f180c980924f834392660ce30ace
sabrasnider <sabra.snider@s2shape.com>
2021-03-26 22:19:30 -0400

    whoops let's try that again

--- SabrasProject/Program.cs
+++ SabrasProject/Program.cs
@@ -8,10 +8,18 @@ namespace SabrasProject
     {
         static void Main(string[] args)
         {
+            var something = new Methods();
             Console.WriteLine("Hello World!");
-
+            var green = new List<FileInfo>();
+            var pink = something.Beep();
+            var thing = something.Boop();
+            //This is where Zhu Li does the thing.
+            something.ZhuLiDoTheThing();
         }
+    }
 
+    public class Methods
+    {
         public void ZhuLiDoTheThing(DirectoryInfo something, List<FileInfo> things, string s)
         {
             FileInfo[] stuff = null;
@@ -37,11 +45,23 @@ namespace SabrasProject
                 }
             }
             otherstuff = something.GetDirectories();
-
             foreach (DirectoryInfo thing in otherstuff)
             {
                 ZhuLiDoTheThing(thing, things, s);
             }
+        }
+
+        public DirectoryInfo Boop()
+        {
+            Console.WriteLine("Where are we?");
+            var thing = Console.ReadLine();
+            return new DirectoryInfo(thing);
+        }
+
+        public string Beep()
+        {
+            Console.WriteLine("So tell me what you want, what you really really want.");
+            return Console.ReadLine();
         }
     }
 
